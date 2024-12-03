d2579e832e2a4a73789f172952da2756a131e293
zachspriggs <zachspriggs@gmail.com>
2024-12-02 21:15:20 -0800

    finished day 2

--- Day2/day2.go
+++ Day2/day2.go
@@ -1,13 +1,82 @@
-
 package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	data, err := os.ReadFile("day2.txt")
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+	helper(string(data))
+}
+
+func part1(input [][]int) {
+	numSafe := 0
+
+	increasing := false
+	decreasing := false
+	unsafe := false
+
+	for _, arr := range input {
+		if arr[0] > arr[1] {
+			decreasing = true
+		} else {
+			increasing = true
+		}
+		for i := 1; i < len(arr); i++ {
+			diff := math.Abs(float64(arr[i] - arr[i-1]))
+			if diff > 3 || diff < 1 {
+				unsafe = true
+				break
+			}
+			if increasing && arr[i] < arr[i-1] {
+				unsafe = true
+				break
+			}
+			if decreasing && arr[i] > arr[i-1] {
+				unsafe = true
+				break
+			}
+		}
+		if !unsafe {
+			numSafe++
+		}
+		unsafe, increasing, decreasing = false, false, false
+	}
+	fmt.Println(numSafe)
+}
+
+func part2(input string) {
 
 }
 
-func part1(input string){
+func helper(input string) {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
 
-}
+	result := make([][]int, len(lines))
 
-func part2(input string){
+	for i, line := range lines {
+		numStrings := strings.Fields(line)
 
+		numbers := make([]int, len(numStrings))
+
+		for j, numStr := range numStrings {
+			num, err := strconv.Atoi(numStr)
+			if err != nil {
+				log.Printf("error converting string to number: %v", err)
+				continue
+			}
+			numbers[j] = num
+		}
+		result[i] = numbers
+	}
+	part1(result)
 }
