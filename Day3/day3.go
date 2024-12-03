ed71499e55e5884794263ff1600478b083e87a30
zachspriggs <zachspriggs@gmail.com>
2024-12-02 22:33:23 -0800

    learned a little regex to more easily solve this puzzle. Doing it by hand was going to be a pain

--- Day3/day3.go
+++ Day3/day3.go
@@ -1,13 +1,40 @@
-
 package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
 func main() {
+	data, err := os.ReadFile("day3.txt")
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+	part1(string(data))
+}
+
+func part1(input string) {
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	matches := re.FindAllStringSubmatch(input, -1)
+
+	sum := 0
+
+	for _, match := range matches {
+		num1, _ := strconv.Atoi(match[1])
+		num2, _ := strconv.Atoi(match[2])
+		sum += mul(num1, num2)
+	}
+
+	fmt.Println(sum)
+}
+
+func part2(input string) {
 
 }
 
-func part1(input string){
-
-}
-
-func part2(input string){
-
+func mul(num1 int, num2 int) int {
+	return num1 * num2
 }
