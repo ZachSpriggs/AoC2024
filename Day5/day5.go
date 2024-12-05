649f9f6147e13038be84a8a0da147ea594c1dffa
zachspriggs <zachspriggs@gmail.com>
2024-12-04 22:37:51 -0800

    finished day 5 part 1

--- Day5/day5.go
+++ Day5/day5.go
@@ -1,13 +1,92 @@
-
 package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Rule struct {
+	before int
+	after  int
+}
+
 func main() {
+	data, err := os.ReadFile("day5.txt")
+	if err != nil {
+		fmt.Println("error reading file", err)
+		return
+	}
+	part1(string(data))
+}
+
+func part1(input string) {
+	sections := strings.Split(input, "\n\n")
+	rules := parseRules(sections[0])
+	updates := parseUpdates(sections[1])
+
+	sum := 0
+
+	for _, update := range updates {
+		if isValid(update, rules) {
+			midIdx := len(update) / 2
+			sum += update[midIdx]
+		}
+	}
+	fmt.Println(sum)
+}
+
+func part2(input string) {
 
 }
 
-func part1(input string){
+// Helper functions
 
+func parseRules(input string) []Rule {
+	rules := make([]Rule, 0)
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	for _, line := range lines {
+		parts := strings.Split(line, "|")
+		before, _ := strconv.Atoi(strings.TrimSpace(parts[0]))
+		after, _ := strconv.Atoi(strings.TrimSpace(parts[1]))
+		rules = append(rules, Rule{before: before, after: after})
+	}
+	return rules
 }
 
-func part2(input string){
+func parseUpdates(input string) [][]int {
+	updates := make([][]int, 0)
+	lines := strings.Split(strings.TrimSpace(input), "\n")
 
+	for _, line := range lines {
+		update := make([]int, 0)
+		numStrs := strings.Split(line, ",")
+		for _, numStr := range numStrs {
+			num, _ := strconv.Atoi(strings.TrimSpace(numStr))
+			update = append(update, num)
+		}
+		updates = append(updates, update)
+	}
+	return updates
+}
+
+func isValid(update []int, rules []Rule) bool {
+	positions := make(map[int]int)
+	for i, num := range update {
+		positions[num] = i
+	}
+
+	for _, rule := range rules {
+		beforePos, beforeExists := positions[rule.before]
+		afterPos, afterExists := positions[rule.after]
+
+		if beforeExists && afterExists {
+			if beforePos > afterPos {
+				return false
+			}
+		}
+	}
+	return true
 }
