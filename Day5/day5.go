e25afc62ee65189707668dba8a9a2a00689706e2
zachspriggs <zachspriggs@gmail.com>
2024-12-04 22:57:07 -0800

    finished part 2 day 5, this is getting really tough

--- Day5/day5.go
+++ Day5/day5.go
@@ -18,7 +18,7 @@ func main() {
 		fmt.Println("error reading file", err)
 		return
 	}
-	part1(string(data))
+	part2(string(data))
 }
 
 func part1(input string) {
@@ -38,7 +38,21 @@ func part1(input string) {
 }
 
 func part2(input string) {
+	sections := strings.Split(input, "\n\n")
+	rules := parseRules(sections[0])
+	updates := parseUpdates(sections[1])
 
+	sum := 0
+
+	for _, update := range updates {
+		if !isValid(update, rules) {
+			sortedUpdate := sortUpdate(update, rules)
+			middleIdx := len(sortedUpdate) / 2
+			sum += sortedUpdate[middleIdx]
+		}
+	}
+
+	fmt.Println(sum)
 }
 
 // Helper functions
@@ -89,4 +103,54 @@ func isValid(update []int, rules []Rule) bool {
 		}
 	}
 	return true
+}
+
+func sortUpdate(update []int, rules []Rule) []int {
+	dependencies := make(map[int]map[int]bool)
+	numbers := make(map[int]bool)
+
+	for _, num := range update {
+		dependencies[num] = make(map[int]bool)
+		numbers[num] = true
+	}
+
+	for _, rule := range rules {
+		if numbers[rule.before] && numbers[rule.after] {
+			dependencies[rule.after][rule.before] = true
+		}
+	}
+
+	result := make([]int, 0, len(update))
+	visited := make(map[int]bool)
+	temp := make(map[int]bool)
+
+	var visit func(int)
+	visit = func(n int) {
+		if temp[n] {
+			return
+		}
+		if visited[n] {
+			return
+		}
+		temp[n] = true
+
+		for before := range dependencies[n] {
+			visit(before)
+		}
+		temp[n] = false
+		visited[n] = true
+		result = append(result, n)
+	}
+
+	for num := range numbers {
+		if !visited[num] {
+			visit(num)
+		}
+	}
+
+	for i := 0; i < len(result)/2; i++ {
+		result[i], result[len(result)-1-i] = result[len(result)-1-i], result[i]
+	}
+
+	return result
 }
